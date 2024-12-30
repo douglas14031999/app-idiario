de803516b9f65d66a2e08a647e41e046a6b0637a
Eder Soares <edersoares@me.com>
2024-12-30 16:20:33 -0300

    Organiza dados offline

--- src/app/services/offline_data_persistence/offline_data_persister.ts
+++ src/app/services/offline_data_persistence/offline_data_persister.ts
@@ -1,18 +1,18 @@
-import { ConnectionService } from './../connection';
-import { TeachingPlansPersisterService } from './teaching_plans_persister';
-import { LessonPlansPersisterService } from './lesson_plans_persister';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { Observable, concat, of } from 'rxjs';
+import { catchError, concatMap } from 'rxjs/operators';
+import { ClassroomsPersisterService } from './classrooms_persister';
 import { ContentLessonPlansPersisterService } from './content_lesson_plans_persister';
 import { ContentRecordsPersisterService } from './content_records_persister';
 import { DisciplinesPersisterService } from './disciplines_persister';
-import { SchoolCalendarsPersisterService } from './school_calendars_persister';
 import { ExamRulesPersisterService } from './exam_rules_persister';
-import { ClassroomsPersisterService } from './classrooms_persister';
+import { LessonPlansPersisterService } from './lesson_plans_persister';
+import { SchoolCalendarsPersisterService } from './school_calendars_persister';
+import { TeachingPlansPersisterService } from './teaching_plans_persister';
 import { UnitiesPersisterService } from './unities_persister';
-import { User } from './../../data/user.interface';
-import { Storage } from '@ionic/storage-angular';
-import { Injectable } from '@angular/core';
-import { Observable, from, concat, of } from 'rxjs';
-import { catchError, concatMap } from 'rxjs/operators';
+import { ConnectionService } from '../connection';
+import { User } from '../../data/user.interface';
 
 @Injectable()
 export class OfflineDataPersisterService {
@@ -31,18 +31,25 @@ export class OfflineDataPersisterService {
   ) {}
 
   private clearStorage(): void {
-    //this.storage.remove('unities');
-    this.storage.remove('classrooms');
-    this.storage.remove('disciplines');
-    this.storage.remove('examRules');
-    this.storage.remove('schoolCalendars');
-    this.storage.remove('frequencies');
-    this.storage.remove('contentLessonPlans');
-    this.storage.remove('contentRecords');
+
+    // TODO confirmar
+    // Entender se deve ser apagado o storage se o usuário for o mesmo, pois
+    // caso a sessão do usuário tenha se expirado, talvez os dados existentes
+    // ainda não tenham sido sincronizados
+
+    this.storage.remove('classrooms').then();
+    this.storage.remove('contentLessonPlans').then();
+    this.storage.remove('contentRecords').then();
+    this.storage.remove('disciplines').then();
+    this.storage.remove('examRules').then();
+    this.storage.remove('frequencies').then(); // TODO
+    this.storage.remove('lessonPlans').then(); // TODO
+    this.storage.remove('schoolCalendars').then();
+    this.storage.remove('teachingPlans').then(); // TODO
+    this.storage.remove('unities').then();
   }
 
   persist(user: User): Observable<void> {
-    //console.log(user)
     if (this.connectionService.isOnline) {
       this.clearStorage();
     }
