9c84650f0b8cfcaa7106b18aed090024e20342dc
Eder Soares <edersoares@me.com>
2025-01-21 11:35:49 -0300

    Se em ambiente local, não faz a consulta dos municípios

--- src/app/services/customers.ts
+++ src/app/services/customers.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiService } from './api';
 import { Customer } from '../data/customer.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class CustomersService {
@@ -13,6 +14,16 @@ export class CustomersService {
   ) {}
 
   getCustomers(): Observable<Customer[]> {
+    if (!environment.production) {
+      const all = {
+        name: 'Localhost',
+        url: environment.app.cities_url,
+        support_url: environment.app.cities_url,
+      } as Customer;
+
+      return of([all]).pipe();
+    }
+
     return this.http.get<any[]>(this.api.getAllHostsUrl()).pipe(
       map((response: any) => {
         if (response && response.customers) {
